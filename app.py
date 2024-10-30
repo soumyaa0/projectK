4d9b77f3f97c70150719c821d634d04b2278f2ea
soumya <soumyaranjanjena815@gmail.com>
2024-10-30 12:14:17 +0530

    modified app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import boto3
+client = boto3.client('ec2')
+response = client.run_instances(
+    ImageId='ami-0dee22c13ea7a9a67',
+    InstanceType='t2.micro',
+    KeyName = 'projectK',
+    MaxCount = 1,
+    MinCount = 1,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'Boto3-py'  # Replace with your desired instance name
+                }
+            ]
+        }
+    ]
+)
+
